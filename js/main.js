a2345fe28c8caeb6e5332db1f4092d5a83410dd9
dbailey0566 <47046017+dbailey0566@users.noreply.github.com>
2026-02-12 12:27:17 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  // Construct Info Logic
   const constructs = document.querySelectorAll(".construct");
   const panel = document.querySelector("#infoPanel");
   const text = document.querySelector("#infoText");
@@ -22,6 +23,27 @@ document.addEventListener("DOMContentLoaded", function () {
         text.setAttribute("value", info);
         text.setAttribute("visible", true);
 
+      }
+
+    });
+
+  });
+
+  // Alignment Challenge Logic
+  const answers = document.querySelectorAll(".answer");
+
+  answers.forEach(function (answer) {
+
+    answer.addEventListener("click", function () {
+
+      const correct = answer.getAttribute("data-correct");
+
+      answers.forEach(a => a.setAttribute("color", "#DDDDDD"));
+
+      if (correct === "true") {
+        answer.setAttribute("color", "#8BC34A");
+      } else {
+        answer.setAttribute("color", "#F44336");
       }
 
     });
